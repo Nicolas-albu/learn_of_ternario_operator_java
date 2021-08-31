630d95f81b06d918fa1e53255989c0922426164e
nicolas <nicolasalbuquerque581@gmail.com>
2021-08-31 20:58:27 -0300

    upload

---
+++ src/operadorternario/OperadorTernario.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operadorternario;
+
+/**
+ *
+ * @author Andressa
+ */
+public class OperadorTernario {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int n1, n2, r;
+        n1 = 50;
+        n2 = 20;
+        r = (n1 > n2)? 40 : 50;
+        System.out.println(r);
+    }
+    
+}
